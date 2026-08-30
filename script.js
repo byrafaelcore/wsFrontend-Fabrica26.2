5a898b5b7b40eda8f070511caa08a917d11d551c
rafael <dev.rafael13@gmail.com>
2026-08-29 21:18:45 -0300

    fix: nome das variaveis da chamada dos dados da api e o fechamento da tag da div container

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const url = "https://overfast-api.tekrop.fr/heroes";
 
-async function pegarDados() {
-    const resp = await fetch(url)
-    if (resp.status == 200) {
-        const obj = await resp.json();
-        console.log(obj)
-    }
+async function pegarDadosApi() {
+    const respostaApi = await fetch(url)
+    const dadosDosHerois = await respostaApi.json();
+    console.log(dadosDosHerois)
 }
 
-pegarDados();
+pegarDadosApi();
+
