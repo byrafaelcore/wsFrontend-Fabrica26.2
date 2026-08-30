d5ad185fc06c5730bd2f21816ff8d3daade365fa
rafael <dev.rafael13@gmail.com>
2026-08-29 22:33:48 -0300

    feat: colocando as informações dos personagens na aplicação

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const url = "https://overfast-api.tekrop.fr/heroes";
 
 async function pegarDadosApi() {
-    const respostaApi = await fetch(url)
+
+    const respostaApi = await fetch(url);
     const dadosDosHerois = await respostaApi.json();
-    console.log(dadosDosHerois)
+    const container = document.querySelector(".container");
+    
+    dadosDosHerois.forEach(({ portrait, role, name }) => {
+
+        console.log(portrait);
+        console.log(name);
+        console.log(role);
+
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML = `
+            <img src="${portrait}" alt="${name}">
+            <h3>${name}</h3>
+            <p>${role}</p>
+        `;
+
+        console.log(portrait);
+
+        container.appendChild(card);
+    });
 }
 
 pegarDadosApi();
-
